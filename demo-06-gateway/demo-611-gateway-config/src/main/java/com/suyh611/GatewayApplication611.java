f1543d1d24da36d5183d98dbb6c07eeda6965efa
suyh <su_787910081@sina.com>
2021-05-02 22:03:56 +0800

    gateway

---
+++ demo-06-gateway/demo-611-gateway-config/src/main/java/com/suyh611/GatewayApplication611.java
@@ -0,0 +1,11 @@
+package com.suyh611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication611 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication611.class, args);
+    }
+}
