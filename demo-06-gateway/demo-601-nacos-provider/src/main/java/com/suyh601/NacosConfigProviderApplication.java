654cc5b837d9802a36ec596a815d74a6b97ac2ca
suyh <su_787910081@sina.com>
2021-05-02 21:33:07 +0800

    gateway

---
+++ demo-06-gateway/demo-601-nacos-provider/src/main/java/com/suyh601/NacosConfigProviderApplication.java
@@ -0,0 +1,14 @@
+package com.suyh601;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NacosConfigProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigProviderApplication.class, args);
+    }
+
+}
---
+++ demo-06-gateway/demo-601-nacos-provider/src/main/java/com/suyh601/bean/Depart.java
@@ -0,0 +1,11 @@
+package com.suyh601.bean;
+
+import lombok.Data;
+
+@Data
+public class Depart {
+    private Integer id;
+    private String name;
+}
+
+
